0b245eddef01d023d5fa3a393953b8d8357c5534
Austin Schaffer <aschaffer@atlas-sys.com>
2018-07-02 17:17:50 -0400

    Added an auto-deploy script

---
+++ pypi-dist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# run this from the root of the repository directory to post the package to
+# PyPI
+
+[[ -d dist_venv ]] || virtualenv --python=python3 dist_venv
+source dist_venv/bin/activate
+python setup.py sdist bdist_wheel
+twine upload dist/*
+
