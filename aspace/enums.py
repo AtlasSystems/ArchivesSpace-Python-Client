2edc7c1322af49276ef2c01c6e34217b21da6a18
Austin Schaffer <aschaffer@atlas-sys.com>
2018-08-03 09:10:22 -0400

    Changed AgentType enum structure

--- aspace/enums.py
+++ aspace/enums.py
@@ -1,15 +1,26 @@
 import enum
+from collections import namedtuple
 
-class AgentTypeInfo(object):
-    def __init__(self, agent_type: str, api_endpoint: str = None):
-        self.agent_type = agent_type
-        self.api_endpoint = api_endpoint
+
+AgentTypeInfo = namedtuple('AgentTypeInfo', ['agent_type', 'api_endpoint'])
+
 
 class AgentType(enum.Enum):
+    # pylint: disable=E1101
+    @property
+    def agent_type(self) -> str:
+        return self.value.agent_type
+
+    @property
+    def api_endpoint(self) -> str:
+        return self.value.api_endpoint
+    # pylint: enable=E1101
+
     PERSON = AgentTypeInfo('agent_person', '/agents/people')
     FAMILY = AgentTypeInfo('agent_family', '/agents/families')
     CORPORATE_ENTITY = AgentTypeInfo('agent_family', '/agents/families')
     SOFTWARE = AgentTypeInfo('software', '/agents/software')
+
 
 class AgentNameOrder(enum.Enum):
     INVERTED = 'inverted'
