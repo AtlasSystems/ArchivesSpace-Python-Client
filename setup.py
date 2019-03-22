e57eb2ac21badeb7691e13dd367d849c1480436b
Austin Schaffer <aschaffer@atlas-sys.com>
2019-03-22 10:30:23 -0400

    Incremented setup.py version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aspace-client',
-    version='1.0.0',
+    version='2.0.1',
     description='Provides methods and classes that can be used when interacting with the ArchivesSpace API.',
     long_description=long_description,
     long_description_content_type='text/markdown',
